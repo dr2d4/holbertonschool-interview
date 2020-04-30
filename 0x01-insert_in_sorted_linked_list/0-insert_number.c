720de3834fe61177bc05059ae3e92d17ece99a6b
Daniel E Rodriguez <dr2d4@live.com>
2020-04-29 20:08:39 -0500

    Cmabiando calloc

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -32,7 +32,7 @@ listint_t *insert_node(listint_t **head, int number)
 
 	if (head)
 	{
-		new_node = calloc(sizeof(listint_t), sizeof(char));
+		new_node = malloc(sizeof(listint_t));
 		new_node->n = number;
 
 		if (!(*head))
